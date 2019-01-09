e057ce27529d4d4a60418031ce7107f8dc0923b8
Kuseru <sergej.herdt.sh@gmail.com>
2019-01-09 20:44:52 +0100

    added annotation

--- src/main/java/org/discordlist/spotifymicroservices/response/StandardResponse.java
+++ src/main/java/org/discordlist/spotifymicroservices/response/StandardResponse.java
@@ -2,6 +2,7 @@ package org.discordlist.spotifymicroservices.response;
 
 import com.google.gson.JsonElement;
 
+@SuppressWarnings("unused")
 public class StandardResponse {
 
     private StatusResponse status;

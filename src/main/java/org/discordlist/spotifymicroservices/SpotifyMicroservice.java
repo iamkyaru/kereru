9a66fcbb19c56188b428bd1dc21bd8390da3ede0
Kuseru <sergej.herdt.sh@gmail.com>
2019-01-09 19:18:33 +0100

    added TrackController

--- src/main/java/org/discordlist/spotifymicroservices/SpotifyMicroservice.java
+++ src/main/java/org/discordlist/spotifymicroservices/SpotifyMicroservice.java
@@ -1,67 +1,33 @@
 package org.discordlist.spotifymicroservices;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import okhttp3.*;
+import org.discordlist.spotifymicroservices.controller.TrackController;
 import org.discordlist.spotifymicroservices.entities.Track;
-import org.discordlist.spotifymicroservices.response.StandardResponse;
 import org.discordlist.spotifymicroservices.services.Service;
 import org.discordlist.spotifymicroservices.services.impl.TrackService;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 import static spark.Spark.*;
 
 public class SpotifyMicroservice {
 
-    public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private Service<Track> service = new TrackService();
+    private static Service<Track> service;
 
     private SpotifyMicroservice() {
+        service = new TrackService();
+
         port(1337);
         get("/", (request, response) -> "Hello World");
-
         path("/", () -> before("/*", (request, response) -> response.type("application/json")));
-        post("/tracks", (request, response) -> {
-            Track track = GSON.fromJson(request.body(), Track.class);
-            this.service.add(track);
-            return GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.SUCCESS));
-        });
-        get("/tracks", (request, response)
-                -> GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.SUCCESS, GSON.toJsonTree(this.service.getAll()))));
-        get("/tracks/:id", (request, response)
-                -> GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.SUCCESS, GSON.toJsonTree(this.service.get(request.params(":id"))))));
-        put("/tracks/:id", (request, response) -> {
-            Track track = GSON.fromJson(request.body(), Track.class);
-            Track editedTrack = this.service.edit(track);
-            if (editedTrack != null)
-                return GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.SUCCESS, GSON.toJsonTree(editedTrack)));
-            else
-                return GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.ERROR, GSON.toJson("Track not found or error in edit")));
-        });
-        delete("/tracks/:id", (request, response) -> {
-            this.service.delete(request.params(":id"));
-            return GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.SUCCESS, GSON.toJson("Track deleted")));
-        });
-        options("/tracks/:id", (request, response) -> GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.SUCCESS,
-                (this.service.exists(request.params(":id")) ? GSON.toJson("Track exists") : GSON.toJson("Track doesn't exist")))));
-        postTrack();
+        /* Tracks */
+        post("/tracks", TrackController.POST_TRACK);
+        get("/tracks", TrackController.GET_TRACKS);
+        get("/tracks/:id", TrackController.GET_TRACK);
+        put("/tracks/:id", TrackController.PUT_TRACK);
+        delete("/tracks/:id", TrackController.DELETE_TRACK);
+        options("/tracks/:id", TrackController.OPTIONS_TRACK);
     }
 
-    private void postTrack() {
-        OkHttpClient client = new OkHttpClient();
-        Map<String, Object> map = new HashMap<>();
-        map.put("name", "Test Name");
-        RequestBody requestBody = RequestBody.create(MediaType.get("application/json; charset=utf-8"), "{ \"name\": \"Test Name\", \"id\": \"1\"}");
-        Request request = new Request.Builder().url("http://0.0.0.0:1337/tracks").post(requestBody).build();
-        try (Response response = client.newCall(request).execute()) {
-            System.out.println(Objects.requireNonNull(response.body()).string());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static Service<Track> getTrackService() {
+        return service;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/org/discordlist/spotifymicroservices/controller/TrackController.java
@@ -0,0 +1,52 @@
+package org.discordlist.spotifymicroservices.controller;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.discordlist.spotifymicroservices.SpotifyMicroservice;
+import org.discordlist.spotifymicroservices.entities.Track;
+import org.discordlist.spotifymicroservices.response.StandardResponse;
+import org.discordlist.spotifymicroservices.services.Service;
+import spark.Route;
+
+public class TrackController {
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final Service<Track> service = SpotifyMicroservice.getTrackService();
+
+    public static final Route POST_TRACK = (request, response) -> {
+        if (request.body() == null || request.body().isEmpty())
+            return GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.ERROR, "Empty request body"));
+        Track track = GSON.fromJson(request.body(), Track.class);
+        if (track.getId() == null)
+            return GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.ERROR, "Track not found"));
+        service.add(track);
+        return GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.SUCCESS));
+    };
+
+    public static final Route GET_TRACKS = (request, response)
+            -> GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.SUCCESS, GSON.toJsonTree(service.getCollection())));
+
+    public static final Route GET_TRACK = (request, response)
+            -> GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.SUCCESS, GSON.toJsonTree(service.get(request.params(":id")))));
+
+    public static final Route PUT_TRACK = (request, response) -> {
+        Track track = GSON.fromJson(request.body(), Track.class);
+        System.out.println(track.getId() + "-" + request.body());
+        if (track.getId() == null || track.getId().isEmpty())
+            return GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.ERROR, "Track not found"));
+        Track editedTrack = service.edit(track);
+        if (editedTrack != null)
+            return GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.SUCCESS, GSON.toJsonTree(editedTrack)));
+        else
+            return GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.ERROR, "Track couldn't be edited."));
+    };
+
+    public static final Route DELETE_TRACK = (request, response) -> {
+        service.delete(request.params(":id"));
+        return GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.SUCCESS, "Track deleted"));
+    };
+
+    public static final Route OPTIONS_TRACK = (request, response)
+            -> GSON.toJson(new StandardResponse(StandardResponse.StatusResponse.SUCCESS,
+            (service.exists(request.params(":id")) ? "Track exists" : "Track doesn't exist")));
+}

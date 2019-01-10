9a6397e09fd125ffc655003a6b03ab40247e6c9b
Kuseru <sergej.herdt.sh@gmail.com>
2019-01-10 15:42:15 +0100

    added album endpoint

--- src/main/java/org/discordlist/spotifymicroservices/SpotifyMicroservice.java
+++ src/main/java/org/discordlist/spotifymicroservices/SpotifyMicroservice.java
@@ -1,10 +1,14 @@
 package org.discordlist.spotifymicroservices;
 
+import com.google.gson.Gson;
+import org.discordlist.spotifymicroservices.controller.AlbumController;
 import org.discordlist.spotifymicroservices.controller.PlaylistController;
 import org.discordlist.spotifymicroservices.controller.TrackController;
+import org.discordlist.spotifymicroservices.entities.Album;
 import org.discordlist.spotifymicroservices.entities.Playlist;
 import org.discordlist.spotifymicroservices.entities.Track;
 import org.discordlist.spotifymicroservices.services.Service;
+import org.discordlist.spotifymicroservices.services.impl.AlbumService;
 import org.discordlist.spotifymicroservices.services.impl.PlaylistService;
 import org.discordlist.spotifymicroservices.services.impl.TrackService;
 
@@ -14,13 +18,19 @@ public class SpotifyMicroservice {
 
     private static Service<Track> trackService;
     private static Service<Playlist> playlistService;
+    private static Service<Album> albumService;
 
     private SpotifyMicroservice() {
         trackService = new TrackService();
         playlistService = new PlaylistService();
+        albumService = new AlbumService();
 
         port(1337);
-        get("/", (request, response) -> "Hello World");
+        threadPool(16, 2, 30000);
+        get("/", (request, response) -> {
+            response.status(404);
+            return new Gson().toJson("This endpoint is not available.");
+        });
         path("/", () -> before("/*", (request, response) -> response.type("application/json")));
         /* Tracks */
         post("/tracks", TrackController.POST_TRACK);
@@ -29,7 +39,6 @@ public class SpotifyMicroservice {
         put("/tracks/:id", TrackController.PUT_TRACK);
         delete("/tracks/:id", TrackController.DELETE_TRACK);
         options("/tracks/:id", TrackController.OPTIONS_TRACK);
-
         /* Playlists */
         post("/playlists", PlaylistController.POST_PLAYLIST);
         get("/playlists", PlaylistController.GET_PLAYLISTS);
@@ -39,6 +48,15 @@ public class SpotifyMicroservice {
         put("/playlists/:id", PlaylistController.PUT_PLAYLIST);
         delete("/playlists/:id", PlaylistController.DELETE_PLAYLIST);
         options("/playlists/:id", PlaylistController.OPTIONS_PLAYLIST);
+        /* Albums */
+        post("/albums", AlbumController.POST_ALBUM);
+        get("/albums", AlbumController.GET_ALBUMS);
+        get("/albums/:id", AlbumController.GET_ALBUM);
+        get("/albums/:id/tracks", AlbumController.GET_ALBUM_TRACKS);
+        get("/albums/:id/tracks/:trackId", AlbumController.GET_ALBUM_TRACK);
+        put("/albums/:id", AlbumController.PUT_ALBUM);
+        delete("/albums/:id", AlbumController.DELETE_ALBUM);
+        options("/albums/:id", AlbumController.OPTIONS_ALBUM);
     }
 
     public static Service<Track> getTrackService() {
@@ -47,6 +65,10 @@ public class SpotifyMicroservice {
 
     public static Service<Playlist> getPlaylistService() {
         return playlistService;
+    }
+
+    public static Service<Album> getAlbumService() {
+        return albumService;
     }
 
     public static void main(String[] args) {

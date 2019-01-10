e7a81d2831c9a90819d42f8ed45718919b6d5b76
Kuseru <sergej.herdt.sh@gmail.com>
2019-01-10 16:39:06 +0100

    added artists endpoint

--- src/main/java/org/discordlist/spotifymicroservices/SpotifyMicroservice.java
+++ src/main/java/org/discordlist/spotifymicroservices/SpotifyMicroservice.java
@@ -2,13 +2,16 @@ package org.discordlist.spotifymicroservices;
 
 import com.google.gson.Gson;
 import org.discordlist.spotifymicroservices.controller.AlbumController;
+import org.discordlist.spotifymicroservices.controller.ArtistController;
 import org.discordlist.spotifymicroservices.controller.PlaylistController;
 import org.discordlist.spotifymicroservices.controller.TrackController;
 import org.discordlist.spotifymicroservices.entities.Album;
+import org.discordlist.spotifymicroservices.entities.Artist;
 import org.discordlist.spotifymicroservices.entities.Playlist;
 import org.discordlist.spotifymicroservices.entities.Track;
 import org.discordlist.spotifymicroservices.services.Service;
 import org.discordlist.spotifymicroservices.services.impl.AlbumService;
+import org.discordlist.spotifymicroservices.services.impl.ArtistService;
 import org.discordlist.spotifymicroservices.services.impl.PlaylistService;
 import org.discordlist.spotifymicroservices.services.impl.TrackService;
 
@@ -19,17 +22,20 @@ public class SpotifyMicroservice {
     private static Service<Track> trackService;
     private static Service<Playlist> playlistService;
     private static Service<Album> albumService;
+    private static Service<Artist> artistService;
 
     private SpotifyMicroservice() {
         trackService = new TrackService();
         playlistService = new PlaylistService();
         albumService = new AlbumService();
+        artistService = new ArtistService();
 
         port(1337);
         threadPool(16, 2, 30000);
         get("/", (request, response) -> {
             response.status(404);
-            return new Gson().toJson("This endpoint is not available.");
+            response.type("application/text");
+            return "This endpoint is not available.";
         });
         path("/", () -> before("/*", (request, response) -> response.type("application/json")));
         /* Tracks */
@@ -57,6 +63,15 @@ public class SpotifyMicroservice {
         put("/albums/:id", AlbumController.PUT_ALBUM);
         delete("/albums/:id", AlbumController.DELETE_ALBUM);
         options("/albums/:id", AlbumController.OPTIONS_ALBUM);
+        /* Artists */
+        post("/artists", ArtistController.POST_ARTIST);
+        get("/artists", ArtistController.GET_ARTISTS);
+        get("/artists/:id", ArtistController.GET_ARTIST);
+        get("/artists/:id/top-tracks", ArtistController.GET_ARTISTS_TOP_TRACKS);
+        get("/artists/:id/top-tracks/:trackId", ArtistController.GET_ARTISTS_TOP_TRACK);
+        put("/artists/:id", ArtistController.PUT_ARTIST);
+        delete("/artists/:id", ArtistController.DELETE_ARTIST);
+        options("/artists/:id", ArtistController.OPTIONS_ARTIST);
     }
 
     public static Service<Track> getTrackService() {
@@ -69,6 +84,10 @@ public class SpotifyMicroservice {
 
     public static Service<Album> getAlbumService() {
         return albumService;
+    }
+
+    public static Service<Artist> getArtistService() {
+        return artistService;
     }
 
     public static void main(String[] args) {

ce6fd940f6b3149c873d00e4df6cc4ae4c12f271
Yannick Seeger <foryasee@gmail.com>
2019-01-11 20:42:29 +0100

    Prevent NPE

--- src/main/java/org/discordlist/spotifymicroservices/cache/RedisSession.java
+++ src/main/java/org/discordlist/spotifymicroservices/cache/RedisSession.java
@@ -3,6 +3,8 @@ package org.discordlist.spotifymicroservices.cache;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import javax.annotation.Nullable;
+
 public class RedisSession {
 
     private final JedisPool pool;
@@ -11,8 +13,8 @@ public class RedisSession {
         this(host, port, null);
     }
 
-    public RedisSession(String host, int port, String password) {
-        if (password.isEmpty())
+    public RedisSession(String host, int port, @Nullable String password) {
+        if (password != null && password.isEmpty())
             password = null;
         JedisPoolConfig config = new JedisPoolConfig();
         pool = new JedisPool(config, host, port, 0, password);
